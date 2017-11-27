f1739cf9e261c7e7a3d9df01a6888afebe6ff085
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-27 20:05:59 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,7 +10,7 @@ class StudentsController < ApplicationController
   def create
     @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
     @student.save
-    redirect_to :show
+    redirect_to :index
   end
 
   def show
